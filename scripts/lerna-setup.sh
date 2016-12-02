916f86e465b981ef223315f9ce76639c0bf5fb49
Vojtech Toman <cry.wolf@centrum.cz>
2016-12-02 17:02:40 +0100

    Add script to create symlinks required by Lerna to work

---
+++ scripts/lerna-setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ROOT_PATH="$(dirname $(readlink -f $0))/.."
+
+cd "$ROOT_PATH"
+
+PACKAGES_DIR='packages'
+if [ ! -d "$PACKAGES_DIR" ]; then
+    echo "- Directory '$PACKAGES_DIR' does not exist"
+    echo "=> Creating directory '$PACKAGES_DIR'"
+    mkdir $PACKAGES_DIR
+    echo "+ Directory '$PACKAGES_DIR' created"
+else
+    echo "= Directory '$PACKAGES_DIR' already exists"
+fi
+
+DIRS=(apps domain ui)
+
+for dir in ${DIRS[*]}; do
+    if [ -d "$ROOT_PATH/$dir" ]; then
+        for module_dir in $(ls $dir); do
+            if [ -d "$ROOT_PATH/$dir/$module_dir" ]; then
+                if [ ! -L "$ROOT_PATH/$PACKAGES_DIR/$module_dir" ]; then
+                    echo "=> Creating symlink $PACKAGES_DIR/$module_dir"
+                    ln -s "$ROOT_PATH/$dir/$module_dir" "$ROOT_PATH/$PACKAGES_DIR/$module_dir"
+                    echo "+ Symlink '$PACKAGES_DIR/$module_dir' created"
+                else
+                    echo "= Symlink '$PACKAGES_DIR/$module_dir' already exists"
+                fi
+            fi
+        done
+    fi
+done
