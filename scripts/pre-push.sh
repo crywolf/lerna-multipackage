6dca5a156b15a4d46dfbda4d48e7259764d2dcaf
Vojtech Toman <cry.wolf@centrum.cz>
2016-12-09 11:25:18 +0100

    chore(release): releasing component
    affects: testApp1@1.3.1
    
    Released from sha 986664178d9fc555739a45b52faadc23b9b82ae5
    
    [skip ci]
    Tag for lerna release

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -21,4 +21,4 @@ fi
 
 git push --follow-tags --no-verify # Push without hook (and including tags)
 
-exit 0 # Return 0 just because we don`t want to see npm error message if we return 1. It causes useless but harmless second push.
+exit 1
